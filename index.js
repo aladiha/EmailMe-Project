d2351046acaa0a74113b634efdf619a4d01de6f8
aladiha <aladin.handuklo@gmail.com>
2021-04-26 22:14:37 +0300

    auth flow done

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
-require('./services/passport');
+const passport = require('passport');
 require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURL);
 
 const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
